766aba73caf0217f9f39fe6a1b74c51144c14db8
Eric Boris <ericboris@Erics-MacBook-Pro.local>
2020-11-23 18:55:56 -0800

    implement q2

--- A5/models.py
+++ A5/models.py
@@ -1,4 +1,6 @@
 import nn
+# TODO remove
+import pdb
 
 
 class PerceptronModel(object):
@@ -60,13 +62,16 @@ class PerceptronModel(object):
 
 			# Let x be the features and let y be the expected training result.
 			for x, y in dataset.iterate_once(batch_size):
-				# Let prediction be the perceptron's predicted output.
+				# Let prediction be the perceptron's predicted output value.
 				prediction = self.get_prediction(x)
 
+				# Let expected be the perceptron's expected output value.
+				expected = y.item()
+
 				# If the prediction doesn't match the expected value then
-				if prediction != y.item():
+				if prediction != expected:
 					# update the weights and flag that training should continue.
-					self.w.update(x, y.item())
+					self.w.update(x, expected)
 					trained = False
 			
 
@@ -78,9 +83,32 @@ class RegressionModel(object):
 	"""
 
 	def __init__(self):
-		# Initialize your model parameters here
-		"*** YOUR CODE HERE ***"
-		## TODO Q2
+		# Let the following represent the dimensionalities of the respective layers.
+		layer_0 = 1
+		layer_1 = 350
+		layer_2 = 150	
+		layer_3 = 1	
+		
+		# Let the following serve as controls for tuning the model.
+		self.batch_size = 1
+		self.learning_rate = -0.09
+
+		# Let each wi represent a (n x m) layer of weights in the network
+		# where n is the dimensionality of the previous layer and
+		# where m is the dimensionality of the current layer.
+		self.w1 = nn.Parameter(layer_0, layer_1)
+		self.w2 = nn.Parameter(layer_1, layer_2)
+		self.w3 = nn.Parameter(layer_2, layer_3)
+
+		# Let each bi represent a (1 x m) layer of biases in the network
+		# such that for each i in bi is associated with a hidden layer wi and
+		# such that m is the size of the hidden layer.
+		self.b1 = nn.Parameter(1, layer_1)
+		self.b2 = nn.Parameter(1, layer_2)
+		self.b3 = nn.Parameter(1, layer_3)
+
+		# Let the following hold each layer's weights and biases as a tuple.
+		self.weights_and_biases = [(self.w1, self.b1), (self.w2, self.b2), (self.w3, self.b3)]
 
 	def run(self, x):
 		"""
@@ -91,8 +119,44 @@ class RegressionModel(object):
 		Returns:
 			A node with shape (batch_size x 1) containing predicted y-values
 		"""
-		"*** YOUR CODE HERE ***"
-		## TODO Q2
+		'''
+		x_cross_w1 = nn.matmul(x, self.w1)
+		x_cross_w1_plus_b1 = nn.add_bias(x_cross_w1, self.b1)
+		relu = nn.relu(x_cross_w1_plus_b1)
+		relu_cross_w2 = nn.matmul(relu, self.w2)
+		relu_cross_w2_plus_b2 = nn.add_bias(relu_cross_w2, self.b2)
+		'''
+		# Return the predicted y value y = f(X).
+
+		# Given a starting activation layer X, if X = A_0 and 
+		# if activation layer A_i+1 = relu(A_i * W_i + B_i) where i = [0, n-1).
+		# Then the following computes the function f(X) = A_n-1 * W_n + B_n.
+		i = 0
+		n = len(self.weights_and_biases)
+		a = x
+
+		# Compute A_i+1 = relu(A_i * W_i + B_i) while i < n-1.
+		while i < n - 1:
+			w, b = self.weights_and_biases[i]
+			a_cross_w = nn.matmul(a, w)
+			a_cross_w_plus_b = nn.add_bias(a_cross_w, b)
+			a = nn.relu(a_cross_w_plus_b)
+			'''	
+			print('i= ', i)
+			print('w= ', w, ' b= ', b)
+			'''
+			i += 1
+		#print('\n\n')
+		
+		# Compute f(X) = A_n-1 * W_n + B_n.
+		w, b = self.weights_and_biases[i]
+		a_cross_w = nn.matmul(a, w)
+		a_cross_w_plus_b = nn.add_bias(a_cross_w, b)
+	
+		# The last term represents the predicted y value. 
+		predicted_y = a_cross_w_plus_b
+
+		return predicted_y 
 
 	def get_loss(self, x, y):
 		"""
@@ -104,16 +168,84 @@ class RegressionModel(object):
 				to be used for training
 		Returns: a loss node
 		"""
-		"*** YOUR CODE HERE ***"
-		## TODO Q2
+		# Let the following compute the square loss between the
+		# predicted outputs given x and the expected outputs given x
+		predicted_y = self.run(x)
+		loss = nn.square_loss(predicted_y, y)
+	
+		# and return the result.
+		return loss
 
 	def train(self, dataset):
 		"""
 		Trains the model.
 		"""
-		"*** YOUR CODE HERE ***"
-		## TODO Q2
+		trained = False
 
+		while not trained:
+			trained = True
+			
+			for x, y in dataset.iterate_once(self.batch_size):
+				loss = self.get_loss(x, y)
+				
+				# Continue training if necessary.
+				if loss.item() > 0.02:
+					trained = False	
+				
+					# Let parameters be a flat list of weights and biases, i.e.
+					# parameters = [w1, b1, w2, b2, ..., wn, bn] from [(w1, b1), (w2, b2), ... (wn, bn)]
+					parameters = []
+					for w, b in self.weights_and_biases:
+						parameters.append(w)
+						parameters.append(b)
+
+					'''grad_wrt_w1, grad_wrt_b1, grad_wrt_w2, grad_wrt_b2 = nn.gradients(loss, parameters)'''
+					'''gradients = nn.gradients(loss, parameters)'''
+					gradients = nn.gradients(loss, [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3])
+
+					# Update each of the weights and biases.				
+					self.w1.update(gradients[0], self.learning_rate)
+					self.b1.update(gradients[1], self.learning_rate)
+					self.w2.update(gradients[2], self.learning_rate)
+					self.b2.update(gradients[3], self.learning_rate)
+					self.w3.update(gradients[4], self.learning_rate)
+					self.b3.update(gradients[5], self.learning_rate)
+
+					print('w1= ', self.w1)
+					print('b1= ', self.b1)
+					print('w2= ', self.w2)
+					print('b2= ', self.b2)
+					print('loss', loss)
+					print('\n\n')
+					'''
+					i = 0	
+					for w, b in self.weights_and_biases:
+						print('start w and b ', self.weights_and_biases)
+						w.update(gradients[i], self.learning_rate)
+						i += 1
+						b.update(gradients[i], self.learning_rate)
+						print('end   w and b ', self.weights_and_biases)
+					print('\n\n')
+					'''
+					'''
+					self.weights_and_biases[0][0].update(grad_wrt_w1, self.learning_rate)
+					self.weights_and_biases[0][1].update(grad_wrt_b1, self.learning_rate)
+					self.weights_and_biases[1][0].update(grad_wrt_w2, self.learning_rate)	
+					self.weights_and_biases[0][1].update(grad_wrt_b2, self.learning_rate)
+					'''	
+				'''
+				print('w		', self.w1)
+				print('b		', self.b1)
+				print('w		', self.w2)
+				print('b		', self.b2)
+				print('x		', x)
+				print('y		', y)
+				print('gw		', grad_wrt_w1)
+				print('gb		', grad_wrt_b1)
+				#print('gw		', grad_wrt_w2)
+				#print('gb		', grad_wrt_b2)
+				print('loss		', loss)
+				print('\n\n')	'''
 
 class DigitClassificationModel(object):
 	"""
